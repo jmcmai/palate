e4b506fed555e880792418cbfe290523d3993e47
Caroline Van <cmvan@stanford.edu>
2024-02-17 06:53:55 -0800

    store user info at login

--- src/convex/_generated/api.d.ts
+++ src/convex/_generated/api.d.ts
@@ -15,6 +15,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as ingredients from "../ingredients.js";
+import type * as users from "../users.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -26,6 +27,7 @@ import type * as ingredients from "../ingredients.js";
  */
 declare const fullApi: ApiFromModules<{
   ingredients: typeof ingredients;
+  users: typeof users;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
