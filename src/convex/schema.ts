f6d5d0de1f17909b7db6c398ef1acea7ef681fca
Caroline Van <cmvan@stanford.edu>
2024-02-17 16:19:43 -0800

    update users schema

--- src/convex/schema.ts
+++ src/convex/schema.ts
@@ -5,6 +5,16 @@ export default defineSchema({
   users: defineTable({
     name: v.string(),
     tokenIdentifier: v.string(),
+    friends: v.array(v.string()),
+    liked_ingredients: v.array(v.string()),
+    disliked_ingredients: v.array(v.string()),
+    restrictions: v.array(v.string()),
+    events: v.array(v.string()),
+    recipes: v.object({
+      liked: v.array(v.string()),
+      disliked: v.array(v.string()),
+    }),
+    pinned: v.array(v.string()),
   }).index("by_token", ["tokenIdentifier"]),
   messages: defineTable({
     body: v.string(),
