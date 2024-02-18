e6b318b669dd8ae6137888093242b6240f74011c
ngochtran <ngochtran2015@gmail.com>
2024-02-17 22:57:16 -0800

    Update api.d.ts

--- src/convex/_generated/api.d.ts
+++ src/convex/_generated/api.d.ts
@@ -16,9 +16,6 @@ import type {
 } from "convex/server";
 import type * as dishes from "../dishes.js";
 import type * as events from "../events.js";
-import type * as messages from "../messages.js";
-import type * as tavily from "../tavily.js";
-import type * as together from "../together.js";
 import type * as users from "../users.js";
 
 /**
@@ -32,9 +29,6 @@ import type * as users from "../users.js";
 declare const fullApi: ApiFromModules<{
   dishes: typeof dishes;
   events: typeof events;
-  messages: typeof messages;
-  tavily: typeof tavily;
-  together: typeof together;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
